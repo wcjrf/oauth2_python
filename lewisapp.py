aafbdb82a4047ac3362bf11d92d309bec9fa4725
wcjrf <37749046+wcjrf@users.noreply.github.com>
2018-03-27 16:18:19 -0700

    1

--- lewisapp.py
+++ lewisapp.py
@@ -1,8 +1,8 @@
 from pyoauth2 import Client
 
-KEY = '7e2d747e91a7c32b0f9af8c60c8864f2bc075ed6117f713980584c40c826f46a'
-SECRET = '5537a8755dd49e2ab5cdcb4ec70349080d4b44da0464033f5df619bf122376a7'
-CALLBACK = 'https://lewistest2.herokuapp.com/callback'
+KEY = ''
+SECRET = ''
+CALLBACK = ''
 
 client = Client(KEY, SECRET,
                 site='https://app.procore.com',
